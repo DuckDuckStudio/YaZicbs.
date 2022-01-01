3872a639195a443069981396b4cc11a73f0c7884
Duck Publishing House <yzcbs@outlook.com>
2022-01-01 18:58:21 +0800

    Create Come on!!!.bat

---
+++ Come on!!!.bat
@@ -0,0 +1,22 @@
+goto start
+ = 癫痫警告
+ = Epilepsy warning
+ = 咕咕咕~
+ :start
+ color 
+echo 嗨起来!!!
+color 1a
+echo
+color 2b
+echo
+color 3c
+echo
+color 4d
+echo 
+color 5e
+echo
+color 6f
+echo
+color 70
+echo
+%0
