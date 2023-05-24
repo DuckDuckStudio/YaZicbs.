bfefa0d1dd013a3ede35d16d4179232321fff37e
Duck Studio <Yzcbs123@163.com>
2023-05-24 17:02:19 +0800

    Add files via upload

---
+++ DuckStudio/c++/7-10.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main () {
+	long long q,m=1;
+	long long t=2;
+	bool isPrime=false;
+	cin>>q;
+	//cout<<endl;
+	for(;q>0;q--){
+		cin>>m;
+		for (;t<m;t++){
+			if (m%t==0) {
+				isPrime=true;
+				break;
+			}
+		}
+		if (isPrime) {
+			cout<<"No";
+		}else{
+			cout<<"Yes";
+		}
+		cout<<endl;
+		isPrime=false;
+    }
+    return 0; 
+}
